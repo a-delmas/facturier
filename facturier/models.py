4c2fbf3849cbf8b43aee2331f69925be506ca285
a-delmas <aurore.delmas@hotmail.fr>
2020-02-11 10:13:24 +0100

    second

--- facturier/models.py
+++ facturier/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Client(models.Model):
+
+    def __str__(self):
+        return self.raison_sociale
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    tel = models.IntegerField(max_length=10)
+    raison_sociale = models.CharField(max_length=100)
+    siret = models.IntegerField(max_length=14)
+    tva = models.CharField(max_length=13)
+    email = models.CharField(max_length=50)
